2ef60d9064f568e641665ac0194225f0658a467c
Thomas Allen <gtallenpadi13@gmail.com>
2025-04-05 14:23:28 -0600

    Adding lesson 1 review exercises

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const userInput = prompt("Please a number: ")
-console.log(userInput) ;
+const height = prompt("Please the HEIGHT of the triangle: ")
+const base = prompt("Please enter the BASE of the triangle: ")
+const areaTriangle = (height*base)/2
+
+console.log(`Height entered ${height}`);
+console.log(`Base entered ${base}`);
+console.log(`Area of triangle entered ${areaTriangle}`);
+
+
 
